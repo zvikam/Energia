d3e06a129cc52b02fb1aeb3a238b0ce1f3e31f1c
dberenguer <dberenguer@usapiens.com>
2013-11-05 11:39:31 +0100

    New pin distribution

--- hardware/msp430/variants/panStamp/pins_energia.h
+++ hardware/msp430/variants/panStamp/pins_energia.h
@@ -36,20 +36,22 @@
 #define BV(x) (1 << (x))
 #endif
 
-#if defined(__MSP430_HAS_USCI_A0__)
-static const uint8_t DEBUG_UARTRXD = 14;  /* Receive  Data (RXD) at P3.7 */
-static const uint8_t DEBUG_UARTTXD = 15;  /* Transmit Data (TXD) at P3.6 */
+static const uint8_t DEBUG_UARTTXD = 6;  /* Transmit Data (TXD) at P1.6 */
+static const uint8_t DEBUG_UARTRXD = 7;  /* Receive  Data (RXD) at P1.7 */
 #define DEBUG_UARTRXD_SET_MODE (PORT_SELECTION0 | PORT_SELECTION1 | INPUT)
 #define DEBUG_UARTTXD_SET_MODE (PORT_SELECTION0 | PORT_SELECTION1 | OUTPUT)
-#endif
 
-#if defined(__MSP430_HAS_USCI_B0__)
-//Only set up for I2C right now
-static const uint8_t TWISDA  = 4; /* P1.7 */
-static const uint8_t TWISCL  = 5; /* P1.6 */
+static const uint8_t TWISDA  = 4; /* P1.4 */
+static const uint8_t TWISCL  = 5; /* P1.5 */
 #define TWISDA_SET_MODE  (PORT_SELECTION0 | PORT_SELECTION1 | INPUT_PULLUP)
 #define TWISCL_SET_MODE  (PORT_SELECTION0 | PORT_SELECTION1 | INPUT_PULLUP)
-#endif
+
+static const uint8_t MOSI    = 3; /* P1.7 */
+static const uint8_t MISO    = 4; /* P1.6 */
+static const uint8_t SCK     = 5;  /* P1.5 */
+#define SPISCK_SET_MODE (PORT_SELECTION0 | PORT_SELECTION1)
+#define SPIMOSI_SET_MODE (PORT_SELECTION0 | PORT_SELECTION1)
+#define SPIMISO_SET_MODE (PORT_SELECTION0 | PORT_SELECTION1)
 
 // Debug UART is on USCIA0 so offset is 0
 #define DEBUG_UART_MODULE_OFFSET 0x0
@@ -61,47 +63,54 @@ static const uint8_t A3  = 3;
 static const uint8_t A4  = 4;
 static const uint8_t A5  = 5;  // Connected to onboard 10K NTC
 
-//                      +----------+
-//               GND   1|   ||||   |24  GND
-//               P3.0  2|          |23  P1.0
-//               P3.1  3|          |22  P1.1
-//               P3.2  4|          |21  P1.2
-//               P2.7  5|          |20  P1.3 (SPI_MOSI)
-//               P2.6  6|          |19  P1.4 (SPI_MISO / I2C_SDA)
-//               GND   7|          |18  P1.5 (SPI_SCK / I2C_SCL)
-//          (A4) P2.4  8|          |17  P1.6 (UART_TX)
-//          (A3) P2.3  9|          |16  P1.7 (UART_RX)
-//          (A2) P2.2 10|          |15  GND
-//          (A1) P2.1 11|          |14  VCC
-//          (A0) P2.0 12|          |13  RST
-//                      +----------+
+//                      +-----------+
+//               GND   1|    ||||   |24  GND
+//         (D16) P3.0  2|           |23  P1.0 (D0)
+//         (D17) P3.1  3|           |22  P1.1 (D1)
+//         (D18) P3.2  4|           |21  P1.2 (D2)
+//         (D15) P2.7  5|           |20  P1.3 (SPI_MOSI) (D3)
+//         (D14) P2.6  6|           |19  P1.4 (SPI_MISO / I2C_SDA) (D4)
+//               GND   7|           |18  P1.5 (SPI_SCK / I2C_SCL) (D5)
+//      (D12/A4) P2.4  8|           |17  P1.6 (UART_TX) (D6)
+//      (D11/A3) P2.3  9|           |16  P1.7 (UART_RX) (D7)
+//      (D10/A2) P2.2 10|           |15  GND
+//       (D9/A1) P2.1 11|           |14  VCC
+//       (D8/A0) P2.0 12|           |13  RST
+//                      +-----------+
+//                      | | | | | | |
+//                   P2.0 | | | | | RST
+//                      VCC | | | TST
+//                        GND | TX
+//                            RX
 //
+//                  TEMPSENSOR  -- P2.5
+//                  ONBOARD_LED -- PJ.1
 
 // Pin names based on the silkscreen
 //
-static const uint8_t P3_0 = 2;
-static const uint8_t P3_1 = 3;
-static const uint8_t P3_2 = 4;
+static const uint8_t P3_0 = 16;
+static const uint8_t P3_1 = 17;
+static const uint8_t P3_2 = 18;
 
-static const uint8_t P2_7 = 5;
-static const uint8_t P2_6 = 6;
-static const uint8_t P2_4 = 8;
-static const uint8_t P2_3 = 9;
+static const uint8_t P2_7 = 15;
+static const uint8_t P2_6 = 14;
+static const uint8_t P2_4 = 12;
+static const uint8_t P2_3 = 11;
 static const uint8_t P2_2 = 10;
-static const uint8_t P2_1 = 11;
-static const uint8_t P2_0 = 12;
+static const uint8_t P2_1 = 9;
+static const uint8_t P2_0 = 8;
 
-static const uint8_t P1_7 = 16;
-static const uint8_t P1_6 = 17;
-static const uint8_t P1_5 = 18;
-static const uint8_t P1_4 = 19;
-static const uint8_t P1_3 = 20;
-static const uint8_t P1_2 = 21;
-static const uint8_t P1_1 = 22;
-static const uint8_t P1_0 = 23;
+static const uint8_t P1_7 = 7;
+static const uint8_t P1_6 = 6;
+static const uint8_t P1_5 = 5;
+static const uint8_t P1_4 = 4;
+static const uint8_t P1_3 = 3;
+static const uint8_t P1_2 = 2;
+static const uint8_t P1_1 = 1;
+static const uint8_t P1_0 = 0;
 
-static const uint8_t TEMPSENSOR = 25;
-static const uint8_t ONBOARD_LED = 26;
+static const uint8_t TEMPSENSOR = 13;
+static const uint8_t ONBOARD_LED = 19;
 
 
 
@@ -161,89 +170,73 @@ const uint16_t port_to_pmap[] = {
  * T0A1, T1A1 and T1A2 
  */
 const uint8_t digital_pin_to_timer[] = {
-	NOT_ON_TIMER, /*  dummy */
-	NOT_ON_TIMER, /*  1 - GND */
-	NOT_ON_TIMER, /*  2 - P3.0 */
-	NOT_ON_TIMER, /*  3 - P3.1 */
-	NOT_ON_TIMER, /*  4 - P3.2 */
-	T1A2,         /*  5 - P2.7 */
-	T1A1,         /*  6 - P2.6 */
-	NOT_ON_TIMER, /*  7 - GND */
-	T1A0,         /*  8 - P2.4 */
-	NOT_ON_TIMER, /*  9 - P2.3 */
-	NOT_ON_TIMER, /* 10 - P2.2 */
-	NOT_ON_TIMER, /* 11 - P2.1 */
-	NOT_ON_TIMER, /* 12 - P2.0 */
-	NOT_ON_TIMER, /* 13 - RST */
-	NOT_ON_TIMER, /* 14 - VCC */
-	NOT_ON_TIMER, /* 15 - GND */
-	NOT_ON_TIMER, /* 16 - P1.7 */  
-	NOT_ON_TIMER, /* 17 - P1.6 */  
-	NOT_ON_TIMER, /* 18 - P1.5 */
-	T0A4,         /* 19 - P1.4 */
-	T0A3,         /* 20 - P1.3 */
-	T0A2,         /* 21 - P1.2 */
-	T0A1,         /* 22 - P1.1 */
-	T0A0,         /* 23 - P1.0 */
-	NOT_ON_TIMER, /* 24 - GND */
+	T0A0,         /* P1.0 */
+	T0A1,         /* P1.1 */
+	T0A2,         /* P1.2 */
+	T0A3,         /* P1.3 */
+	T0A4,         /* P1.4 */
+	NOT_ON_TIMER, /* P1.5 */
+	NOT_ON_TIMER, /* P1.6 */
+	NOT_ON_TIMER, /* P1.7 */
+	NOT_ON_TIMER, /* P2.0 */
+	NOT_ON_TIMER, /* P2.1 */
+	NOT_ON_TIMER, /* P2.2 */
+	NOT_ON_TIMER, /* P2.3 */
+	T1A0,         /* P2.4 */
+	NOT_ON_TIMER, /* TEMPSENSOR */
+	T1A1,         /* P2.6 */
+	NOT_ON_TIMER, /* P2.7 */
+	NOT_ON_TIMER, /* P3.0 */
+	NOT_ON_TIMER, /* P3.1 */
+	NOT_ON_TIMER, /* P3.2 */
+	NOT_ON_TIMER, /* ONBOARD_LED */
 };
 
 const uint8_t digital_pin_to_port[] = {
-	NOT_A_PIN,	 /*  dummy */
-	NOT_A_PIN,   /*  1 - GND  */
-	P3,          /*  2 - P3.0 */
-	P3,          /*  3 - P3.1 */
-	P3,		       /*  4 - P3.2 */
-	P2,          /*  5 - P2.7 */
-	P2,          /*  6 - P2.6 */
-	NOT_A_PIN,   /*  7 - GND */
-	P2,          /*  8 - P2.4 */
-	P2,          /*  9 - P2.3 */
-	P2,          /* 10 - P2.2 */
-	P2,          /* 11 - P2.1 */
-	P2,          /* 12 - P2.0 */
-	NOT_A_PIN,   /* 13 - RST */
-	NOT_A_PIN,   /* 14 - VCC */
-	NOT_A_PIN,   /* 15 - GND */
-	P1,          /* 16 - P1.7 */  
-	P1,          /* 17 - P1.6 */  
-	P1,          /* 18 - P1.5 */
-	P1,          /* 19 - P1.4 */
-	P1,          /* 20 - P1.3 */
-	P1,          /* 21 - P1.2 */
-	P1,          /* 22 - P1.1 */
-	P1,          /* 23 - P1.0 */
-	NOT_A_PIN,   /* 24 - GND */
+	P1,           /* P1.0 */
+	P1,           /* P1.1 */
+	P1,           /* P1.2 */
+	P1,           /* P1.3 */
+	P1,           /* P1.4 */
+	P1,           /* P1.5 */
+	P1,           /* P1.6 */
+	P1,           /* P1.7 */
+	P2,           /* P2.0 */
+	P2,           /* P2.1 */
+	P2,           /* P2.2 */
+	P2,           /* P2.3 */
+	P2,           /* P2.4 */
+	P2,           /* TEMPSENSOR */
+	P2,           /* P2.6 */
+	P2,           /* P2.7 */
+	P3,           /* P3.0 */
+	P3,           /* P3.1 */
+	P3,           /* P3.2 */
+	PJ,           /* ONBOARD_LED */
 };
 
 const uint8_t digital_pin_to_bit_mask[] = {
-	NOT_A_PIN,	 /*  dummy */
-	NOT_A_PIN,   /*  1 - GND  */
-	BV(0),       /*  2 - P3.0 */
-	BV(1),       /*  3 - P3.1 */
-	BV(2),       /*  4 - P3.2 */
-	BV(7),       /*  5 - P2.7 */
-	BV(6),       /*  6 - P2.6 */
-	NOT_A_PIN,   /*  7 - GND */
-	BV(4),       /*  8 - P2.4 */
-	BV(3),       /*  9 - P2.3 */
-	BV(2),       /* 10 - P2.2 */
-	BV(1),       /* 11 - P2.1 */
-	BV(0),       /* 12 - P2.0 */
-	NOT_A_PIN,   /* 13 - RST */
-	NOT_A_PIN,   /* 14 - VCC */
-	NOT_A_PIN,   /* 15 - GND */
-	BV(7),       /* 16 - P1.7 */  
-	BV(6),       /* 17 - P1.6 */  
-	BV(5),       /* 18 - P1.5 */
-	BV(4),       /* 19 - P1.4 */
-	BV(3),       /* 20 - P1.3 */
-	BV(2),       /* 21 - P1.2 */
-	BV(1),       /* 22 - P1.1 */
-	BV(0),       /* 23 - P1.0 */
-	NOT_A_PIN,   /* 24 - GND */
+	BV(0),        /* P1.0 */
+	BV(1),        /* P1.1 */
+	BV(2),        /* P1.2 */
+	BV(3),        /* P1.3 */
+	BV(4),        /* P1.4 */
+	BV(5),        /* P1.5 */
+	BV(6),        /* P1.6 */
+	BV(7),        /* P1.7 */
+	BV(0),        /* P2.0 */
+	BV(1),        /* P2.1 */
+	BV(2),        /* P2.2 */
+	BV(3),        /* P2.3 */
+	BV(4),        /* P2.4 */
+	BV(5),        /* TEMPSENSOR */
+	BV(6),        /* P2.6 */
+	BV(7),        /* P2.7 */
+	BV(0),        /* P3.0 */
+	BV(1),        /* P3.1 */
+	BV(2),        /* P3.2 */
+	BV(1),        /* ONBOARD_LED */
 };
-
 
 #endif
 #endif
