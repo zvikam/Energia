fdf66689a998218dace32b563a3160044483060a
dberenguer <dberenguer@usapiens.com>
2014-04-01 17:48:36 +0200

    panstamp gdb bootloader folder renamed to gdb

---
+++ hardware/msp430/bootloaders/gdb/gdb_bootloader/panstamp_release/pmm.d
@@ -0,0 +1,19 @@
+panstamp_release/pmm.o: pmm.cpp \
+ /home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/msp430.h \
+ /home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/cc430f5137.h \
+ /home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/iomacros.h \
+ /home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/in430.h \
+ /home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/intrinsics.h \
+ ../include/msp430/core/drivers/pmm.h
+
+/home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/msp430.h:
+
+/home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/cc430f5137.h:
+
+/home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/iomacros.h:
+
+/home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/in430.h:
+
+/home/daniel/development/Energia/hardware/tools/msp430/bin/../lib/gcc/msp430/4.6.3/../../../../msp430/include/intrinsics.h:
+
+../include/msp430/core/drivers/pmm.h:
