c403b1ad12bf46d549a1285ac8652cf909af956f
dberenguer <dberenguer@usapiens.com>
2014-05-26 20:33:37 +0200

    Declared defines as long integers

--- sketches/rgbdriver/product.h
+++ sketches/rgbdriver/product.h
@@ -28,22 +28,22 @@
 /**
  * Hardware version
  */
-#define HARDWARE_VERSION         0x00000100
+#define HARDWARE_VERSION         0x00000100L
 
 /**
  * Firmware version
  */
-#define FIRMWARE_VERSION         0x00000100
+#define FIRMWARE_VERSION         0x00000100L
 
 /**
  * Manufacturer SWAP ID
  */
-#define SWAP_MANUFACT_ID        0x00000001
+#define SWAP_MANUFACT_ID         0x00000001L
 
 /**
  * Product SWAP ID
  */
-#define SWAP_PRODUCT_ID         0x00000003
+#define SWAP_PRODUCT_ID          0x00000003L
 
 #endif
 
